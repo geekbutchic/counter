4575dc3870d47c6e8e0f734b338b0d483b6df940
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-10-26 00:22:12 -0400

    counter in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {//INHERIT PROPERTIES
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  addCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  minusCount = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    console.log(this.addCount);
+    return (
+      //RETURNING JSX
+      <React.Fragment>
+        <h1 style={{ textAlign: "center" }}>Navbar</h1>
+        <div style={{ textAlign: "center", marginTop: "30%" }}>
+          <div>Counter : {this.state.count}</div>
+          <button
+            onClick={this.minusCount}
+            style={{
+              borderRadius: "5px",
+              borderWidth: "thin",
+              width: "50px",
+              marginRight: "10px",
+              marginTop: "10px",
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={this.addCount}
+            style={{ width: "50px", borderRadius: "5px", borderWidth: "thin" }}
+          >
+            +
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
